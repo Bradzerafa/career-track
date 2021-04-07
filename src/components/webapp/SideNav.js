b1481ed6493fa62f8d860f54f1ef4d4f108cd6f9
Brad <bradleyzerafa1@hotmail.com>
2021-04-07 15:06:42 +1000

    Basic Routing added for Profile and finance pages.

--- src/components/webapp/SideNav.js
+++ src/components/webapp/SideNav.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import UserProfile from "./UserProfile";
+import UserFinance from "../finance/UserFinance";
 
 class SideNav extends React.Component {
   render() {
-    return <div>SideNav</div>;
+    return (
+      <div>
+        <BrowserRouter>
+          <Link to="userProfile">My Profile</Link>
+          <Link to="userFinance">Financial</Link>
+          <Route path="/userProfile" exact component={UserProfile} />
+          <Route path="/userFinance" exact component={UserFinance} />
+        </BrowserRouter>
+      </div>
+    );
   }
 }
 
