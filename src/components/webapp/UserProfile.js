04c1139475182ba0c5bf1093730f9102bb437506
Brad <bradleyzerafa1@hotmail.com>
2021-04-07 16:02:51 +1000

    Basic user profile created with fakerJS (No Styling added)

--- src/components/webapp/UserProfile.js
+++ src/components/webapp/UserProfile.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import faker from "faker";
 
 class UserProfile extends React.Component {
+  state = {
+    name: "",
+    country: "",
+    currJob: "",
+    currSalary: "",
+    currGoal: "",
+  };
+
+  componentDidMount() {
+    this.setState({
+      name: faker.name.findName(),
+      country: faker.address.country(),
+      currJob: faker.name.jobTitle(),
+      currSalary: "60,000",
+      currGoal: faker.random.float(),
+    });
+    console.log("Hello");
+  }
+
   render() {
-    return <div>User Profile</div>;
+    return (
+      <div>
+        <h1>Welcome to Your Profile</h1>
+        <img src={faker.internet.avatar()} alt="Profile Picture of Person" />
+        <p>Name: {this.state.name}</p>
+        <p>Country: {this.state.country}</p>
+        <p>Current Job: {this.state.currJob}</p>
+        <p>Current Salary: ${this.state.currSalary} AUD</p>
+        <p>Current Savings Goal: ${this.state.currGoal} AUD</p>
+      </div>
+    );
   }
 }
 
